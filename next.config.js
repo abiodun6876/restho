de87c28d03a9d3cde1703920e6bf32fab1f7018f
AKINPELU Abiodun Moses <70350694+abiodun6876@users.noreply.github.com>
2023-09-18 04:19:42 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['restho-nextjs.vercel.app','g.foolcdn.com','fthmb.tqn.com'],
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 's3.amazonaws.com',
-          port: '',
-          pathname: '/my-bucket/**',
-        },
-      ],
+      domains: ['restho-nextjs.vercel.app','g.foolcdn.com','fthmb.tqn.com','wallpapercave.com'],
+      
     },
   };
+
   
   module.exports = nextConfig;
   
