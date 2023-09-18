43dec3b79d54c9b725ff6afb3e6c5bcdcab5b9a8
AKINPELU Abiodun Moses <70350694+abiodun6876@users.noreply.github.com>
2023-09-18 01:21:12 -0700

    Update NewItemSlider.js

--- app/NewItemSlider.js
+++ app/NewItemSlider.js
@@ -1,54 +1,25 @@
 'use client'
 import React, { useState } from 'react';
+import Image from 'next/image'; // Import the Image component from next/image
 import './NewItemSlider.css';
 
 const newItemData = [
-    {
-      title: 'Prawn Fried',
-      description: 'It is a long established fact that a reader will be distracted.',
-      price: '$32',
-      image: 'https://restho-nextjs.vercel.app/assets/images/bg/h2-regular-items12.png',
-    },
-    {
-      title: 'Prawn with Noodles',
-      description: 'It is a long established fact that a reader will be distracted.',
-      price: '$32',
-      image: 'https://restho-nextjs.vercel.app/assets/images/bg/h2-regular-items13.png',
-    },
-  
-  ];
+  {
+    title: 'Prawn Fried',
+    description: 'It is a long established fact that a reader will be distracted.',
+    price: '$32',
+    image: 'https://restho-nextjs.vercel.app/assets/images/bg/h2-regular-items12.png',
+  },
+  {
+    title: 'Prawn with Noodles',
+    description: 'It is a long established fact that a reader will be distracted.',
+    price: '$32',
+    image: 'https://restho-nextjs.vercel.app/assets/images/bg/h2-regular-items13.png',
+  },
+];
 
 function NewItemSlider() {
   const [currentItem, setCurrentItem] = useState(0);
-
-  const nextItem = () => {
-    setCurrentItem((prevItem) => (prevItem + 1) % newItemData.length);
-  };
-
-  const prevItem = () => {
-    setCurrentItem((prevItem) => (prevItem - 1 + newItemData.length) % newItemData.length);
-  };
-
-  const subHeaderStyle = {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center', // Center horizontally
-    marginBottom: '20px',
-    color:'white',
-
-  };
-
-  
-
-  
-
-  const titleStyle = {
-    fontSize: '28px',
-    fontWeight: 'bold',
-    color: 'black',
-    marginBottom: '20px',
-    color:'white',
-  };
 
   const sliderItemStyle = {
     flex: '1',
@@ -74,9 +45,35 @@ function NewItemSlider() {
     margin: '10px 0', // Add margin between cards on mobile
   };
 
+  const nextItem = () => {
+    setCurrentItem((prevItem) => (prevItem + 1) % newItemData.length);
+  };
+
+  const prevItem = () => {
+    setCurrentItem((prevItem) => (prevItem - 1 + newItemData.length) % newItemData.length);
+  };
+
+  const subHeaderStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: '20px',
+    color: 'white',
+  };
+
+  const titleStyle = {
+    fontSize: '28px',
+    fontWeight: 'bold',
+    color: 'black',
+    marginBottom: '20px',
+    color: 'white',
+  };
+
+  // Rest of your component code
+
   return (
     <div className="new-item-slider">
-      <div className="itembanner">
+         <div className="itembanner">
         <div className="itemheader">
           <h4 style={subHeaderStyle}>Our New Items</h4>
           <h1 style={titleStyle}>Restho New Item List</h1>
@@ -84,40 +81,41 @@ function NewItemSlider() {
             Various versions have evolved over the years, sometimes on purpose.
           </h4>
         </div>
-        <div className="slider-content">
-          {newItemData.map((item, index) => (
-            <div
-              key={index}
-              className="slider-item"
-              style={{
-                ...sliderItemStyle,
-                ...(index === currentItem ? activeSliderItemStyle : {}),
-                // Apply responsive styles for mobile
-                ...(window.innerWidth <= 768 ? responsiveSliderItemStyle : {}),
-              }}
-            >
-              <div className="item-info">
-                <div className="rotate-square">
-                  <img src={item.image} alt={item.title} className="item-image" />
-                  <div className="item-details">
-                    <h1>{item.title}</h1>
-                    <p>{item.description}</p>
-                    <p className="price">{item.price}</p>
-                  </div>
+      {/* ... */}
+      <div className="slider-content">
+        {newItemData.map((item, index) => (
+          <div
+            key={index}
+            className="slider-item"
+            style={{
+              ...sliderItemStyle,
+              ...(index === currentItem ? activeSliderItemStyle : {}),
+              // Apply responsive styles for mobile
+              ...(window.innerWidth <= 768 ? responsiveSliderItemStyle : {}),
+            }}
+          >
+            <div className="item-info">
+              <div className="rotate-square">
+                {/* Replace <img> with <Image> */}
+                <Image
+                  src={item.image}
+                  alt={item.title}
+                  className="item-image"
+                  width={250} // Set the desired width
+                  height={250} // Set the desired height
+                />
+                <div className="item-details">
+                  <h1>{item.title}</h1>
+                  <p>{item.description}</p>
+                  <p className="price">{item.price}</p>
                 </div>
               </div>
             </div>
-          ))}
-        </div>
-        <div className="arrow-buttons">
-          <button className="left-arrow" onClick={prevItem}>
-            &lt;
-          </button>
-          <button className="right-arrow" onClick={nextItem}>
-            &gt;
-          </button>
-        </div>
+          </div>
+        ))}
       </div>
+      {/* ... */}
+    </div>
     </div>
   );
 }
