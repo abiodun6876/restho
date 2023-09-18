9913ac5c4b1f7586a5302d49a69bfb4b313a32d4
AKINPELU Abiodun Moses <70350694+abiodun6876@users.noreply.github.com>
2023-09-18 00:09:53 -0700

    Update About.js

--- app/About.js
+++ app/About.js
@@ -1,38 +1,35 @@
 import React from 'react';
+import Image from 'next/image'; // Import the Image component
 import './AboutUs.css'; // Import your CSS file for styling
 
 function AboutUs() {
+  const titleStyle2 = {
+    fontSize: '28px',
+    fontWeight: 'bold',
+    fontFamily: 'Cormorant Garamond,serif',
+    color: 'black',
+    marginBottom: '10px',
+    transition: 'color 0.3s', // Added transition
+  };
 
-    const titleStyle2 = {
-        fontSize: '28px',
-        fontWeight: 'bold',
-        fontFamily: 'Cormorant Garamond,serif',
-        
-        color: 'black',
-        marginBottom: '10px',
-        transition: 'color 0.3s', // Added transition
-      };
-      
-      const descriptionStyle = {
-        fontSize: '18px',
-        fontFamily: 'Arial, sans-serif',
-        color: '#bf9444',
-        marginBottom: '20px',
-        transition: 'color 0.3s', // Added transition
-      };
+  const descriptionStyle = {
+    fontSize: '18px',
+    fontFamily: 'Arial, sans-serif',
+    color: '#bf9444',
+    marginBottom: '20px',
+    transition: 'color 0.3s', // Added transition
+  };
 
-      const subHeaderImageStyle = {
-  width: '30px',
-  height: 'auto',
-  marginRight: '10px',
-  marginLeft: '10px',
-};
+  const subHeaderImageStyle = {
+    width: '30px',
+    height: 'auto',
+    marginRight: '10px',
+    marginLeft: '10px',
+  };
 
   return (
     <div className="about-us-container">
-      <div className="about-us-banner">
-       
-      </div>
+      <div className="about-us-banner"></div>
       <div className="about-us-content">
         <div className="about-us-text">
           <h2 style={titleStyle2}>About Us</h2>
@@ -47,17 +44,21 @@ function AboutUs() {
         </div>
         <div className="about-us-icons">
           <div className="about-icon">
-            <img
+            <Image
               src="https://restho-nextjs.vercel.app/assets/images/icon/h2-about1.svg"
               alt="Experties Chef"
+              width={50} // Set width and height for the Image component
+              height={50}
             />
             <p>Experties Chef</p>
             <p>We serve different types of fresh food.</p>
           </div>
           <div className="about-icon">
-            <img
+            <Image
               src="https://restho-nextjs.vercel.app/assets/images/icon/h2-about2.svg"
               alt="Pure Fresh Food"
+              width={50} // Set width and height for the Image component
+              height={50}
             />
             <p>Pure Fresh Food</p>
             <p>We serve different types of fresh food.</p>
@@ -65,23 +66,31 @@ function AboutUs() {
         </div>
       </div>
       <div className="about-us-image-row">
-        <img
+        <Image
           src="https://restho-nextjs.vercel.app/assets/images/bg/h2-about-img1.png"
           alt="h2-about-img1"
+          width={250} // Set width and height for the Image component
+          height={250}
         />
-        <img
+        <Image
           src="https://restho-nextjs.vercel.app/assets/images/bg/h2-about-img2.png"
           alt="h2-about-img2"
+          width={250} // Set width and height for the Image component
+          height={250}
         />
       </div>
       <div className="about-us-image-row">
-        <img
+        <Image
           src="https://restho-nextjs.vercel.app/assets/images/bg/h2-about-img3.png"
           alt="h2-about-img3"
+          width={250} // Set width and height for the Image component
+          height={250}
         />
-        <img
+        <Image
           src="https://restho-nextjs.vercel.app/assets/images/bg/h2-about-img4.png"
           alt="h2-about-img4"
+          width={250} // Set width and height for the Image component
+          height={250}
         />
       </div>
       <div className="read-more">
